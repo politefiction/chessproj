52737e526f08d25d6a8ab93ee0eab296e7e07696
Chelsi Jones <cajones.bz@gmail.com>
2017-09-17 11:57:18 -0500

    Add tests for each chess piece

--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -1,6 +1,11 @@
 require 'pieces'
 
 describe MoveSet do
+	subject { MoveSet.new([4, 2]) }
+	context 'upon creation' do
+		it { is_expected.to respond_to(:current_square) }
+		it { is_expected.to respond_to(:potential_moves) }
+	end
 end
 
 describe ChessPiece do 
@@ -9,6 +14,13 @@ describe ChessPiece do
 		it { is_expected.to respond_to(:color) }
 		it { is_expected.to respond_to(:token) }
 		it { is_expected.to respond_to(:move_set) }
+	end
+
+	describe '#set_token' do
+		before { subject.set_token("♧", "♣") }
+		it 'assigns the token matching the color' do
+			expect(subject.token).to eq("♣")
+		end
 	end
 end
 
@@ -30,7 +42,7 @@ describe Pawn do
 			subject.first_move = false 
 			subject.pawn_moves
 		end
-		
+
 		it 'should have one potential move' do
 			expect(subject.move_set.potential_moves.length).to eq(1)
 		end
@@ -39,20 +51,70 @@ end
 
 describe Rook do
 	subject { Rook.new("white", [0,0]) }
+	context 'upon creation' do
+		it 'has a token matching its color' do
+			expect(subject.token).to eq("♖")
+		end
+		it 'generates correct potential moves' do
+			pm = subject.move_set.potential_moves
+			expect(pm).to include([5,0], [0,3], [7,0])
+			expect(pm).not_to include(nil)
+		end
+	end
 end
 
 describe Knight do
 	subject { Knight.new("black", [6,7]) }
+	context 'upon creation' do
+		it 'has a token matching its color' do
+			expect(subject.token).to eq("♞")
+		end
+		it 'generates correct potential moves' do
+			pm = subject.move_set.potential_moves
+			expect(pm).to include([5,5], [7,5], [4,6])
+			expect(pm).not_to include(nil)
+		end
+	end
 end
 
 describe Bishop do
 	subject { Bishop.new("white", [0,2]) }
+	context 'upon creation' do
+		it 'has a token matching its color' do
+			expect(subject.token).to eq("♗")
+		end
+		it 'generates correct potential moves' do
+			pm = subject.move_set.potential_moves
+			expect(pm).to include([1,1], [3,5], [4,6])
+			expect(pm).not_to include(nil)
+		end
+	end
 end
 
 describe Queen do
 	subject { Queen.new("black", [3, 7]) }
+	context 'upon creation' do
+		it 'has a token matching its color' do
+			expect(subject.token).to eq("♛")
+		end
+		it 'generates correct potential moves' do
+			pm = subject.move_set.potential_moves
+			expect(pm).to include([3, 4], [2, 7], [6, 4], [1,5])
+			expect(pm).not_to include(nil)
+		end
+	end
 end
 
 describe King do
-	subject { King.new("white") [4, 0]}
+	subject { King.new("white", [4, 0])}
+		context 'upon creation' do
+		it 'has a token matching its color' do
+			expect(subject.token).to eq("♔")
+		end
+		it 'generates correct potential moves' do
+			pm = subject.move_set.potential_moves
+			expect(pm).to include([4, 1], [5, 0], [3, 1])
+			expect(pm).not_to include(nil)
+		end
+	end
 end
