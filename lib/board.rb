b66264896d82265492d5a412df794a12513c7275
Chelsi Jones <cajones.bz@gmail.com>
2017-10-02 09:52:03 -0500

    Make changes for board display

--- lib/board.rb
+++ lib/board.rb
@@ -20,17 +20,17 @@ class ChessBoard
 	def build_board
 		row = (0..7).to_a
 		col = (0..7).to_a
-		row.each { |x| col.each { |y| @squares << Square.new([x, y]) } }
+		row.each { |x| col.each { |y| @squares << Square.new([y, x]) } }
 	end
 
 	def display_board
 		count = 7
 		while count >= 0
-			row = @squares.select { |sq| sq.coord[0] == count }
+			row = @squares.select { |sq| sq.coord[1] == count }
 			puts "#{count + 1}  #{count.even? ? checker_black(row) : checker_white(row)}"
 			count -= 1
 		end
-		puts "    A B C D E F G H"
+		puts "   A B C D E F G H"
 	end
 
 	def checker_white(row)
@@ -39,7 +39,7 @@ class ChessBoard
 			if !sq.piece.nil?
 				ch_row << sq.piece
 			else
-				sq.coord[1].even? ? ch_row << " " : ch_row << "■"
+				sq.coord[0].even? ? ch_row << " " : ch_row << "■"
 			end
 		end
 		ch_row.join(" ")
@@ -51,7 +51,7 @@ class ChessBoard
 			if !sq.piece.nil?
 				ch_row << sq.piece
 			else
-				sq.coord[1].odd? ? ch_row << " " : ch_row << "■"
+				sq.coord[0].odd? ? ch_row << " " : ch_row << "■"
 			end
 		end
 		ch_row.join(" ")
