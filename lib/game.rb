63826475f557f418ddc091124bba2f50a48ed10c
Chelsi Jones <cajones.bz@gmail.com>
2017-10-08 11:05:53 -0500

    Add turn system of sorts

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,10 @@ require_relative 'board'
 require_relative 'pieces'
 require_relative 'player'
 
+# A few notes:
+# Other pieces aren't supposed to be able to actually capture the king.
+# Castling move fixed.
+
 
 class ChessGame
 	attr_accessor :board, :player1, :player2
@@ -9,18 +13,50 @@ class ChessGame
 	def initialize
 		ChessPiece.clear
 		@board = ChessBoard.new
-		@player1 = Player.new("white", @board)
-		@player2 = Player.new("black", @board)
+		@player1 = Player.new("white")
+		@player2 = Player.new("black")
+		pieces_on_board
 	end
 
 	def pieces_on_board
 		@board.squares.each do |sq|
 			ChessPiece.all.each do |pc|
-				if sq.coord == pc.move_set.current_square
+				if sq.coord == pc.current_square
 					sq.piece = pc.token
 				else
-					sq.piece = nil unless pc.occupied? sq.coord
+					sq.piece = nil unless ChessPiece.occupies? sq.coord
 				end
+			end
+		end
+		@board.display_board
+	end
+
+
+	def run_game (current_turn=@player1)
+		2.times { @player1.generate_moves; @player2.generate_moves }
+		king_checkmated?; king_checked?
+		current_turn.turn; puts
+		pieces_on_board; puts
+		if current_turn == @player1
+			@player2.subtract_captured(@player1.captured)
+			run_game(@player2)
+		else
+			@player1.subtract_captured(@player2.captured)
+			run_game
+		end
+	end
+
+	def king_checked?
+		kings = ChessPiece.all.select { |pc| pc.class == King }
+		kings.each { |king| puts "#{king.color.capitalize}'s king is in check!" if king.check? }
+	end
+
+	def king_checkmated?
+		kings = ChessPiece.all.select { |pc| pc.class == King }
+		kings.each do |king|
+			if king.checkmate? 
+				puts "Checkmate! #{king.color == "white" ? "Black wins!" : "White wins!"}"
+				exit
 			end
 		end
 	end
@@ -28,11 +64,15 @@ class ChessGame
 end
 
 
-=begin
+
 
 game = ChessGame.new
-game.pieces_on_board
-game.board.display_board
+#game.pieces_on_board
+game.run_game
 
+
+=begin 
 
 =end
+
+
--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,14 @@
 require_relative 'pieces'
 
 class Player
-	attr_accessor :color, :board, :pieces, :captured
+	attr_accessor :color, :pieces, :captured#, :board
 
-	def initialize(color, board=nil)
+	def initialize(color)
 		@color = color
 		@pieces = []; @captured = []
 		@color == "white" ? generate_pieces(0, 1) : generate_pieces(7, 6)
 		generate_moves
-		match_board(board)
+		#match_board(board)
 	end
 
 	def generate_pieces(a, b)
@@ -31,8 +31,117 @@ class Player
 		end
 	end
 
-	def match_board(board)
-		@board = board
+	def move_piece(from, to)
+		movepc = @pieces.select { |pc| pc.current_square == from }[0]
+		if movepc.potential_moves.include? to
+			if ChessPiece.occupies? to
+				oppospc = ChessPiece.all.select { |pc| pc.current_square == to }[0]
+				oppospc.current_square = nil; oppospc.potential_moves = []
+				@captured << oppospc
+			end
+			castling?(movepc, to) if movepc.class == King
+			movepc.current_square = to
+			movepc.first_move = false if movepc.class == (Pawn || Rook || King)
+		else
+			puts "Please choose a valid move." # maybe have a hint option that shows possible moves?
+			puts "Potential_moves: #{movepc.potential_moves.inspect}"
+			turn
+		end
+	end
+
+	def castling?(king, to)
+		y = king.current_square[1]
+		if king.first_move and king.potential_moves.include? ([1,y] or [6,y])
+			rook1 = ChessPiece.all.select { |pc| pc.class == Rook and pc.current_square == [0, y] }[0]
+			rook2 = ChessPiece.all.select { |pc| pc.class == Rook and pc.current_square == [7, y] }[0]
+			rook1.current_square = [2,y] if to == [1,y]
+			rook2.current_square = [5,y] if to == [6,y]
+		end
+	end
+
+	def turn
+		puts "#{@color.capitalize}\'s turn. Where is the piece you'd like to move? (e.g., 'A4', 'G2', etc.): "
+		fromalpha = gets.chomp; from = convert_coord(fromalpha)
+		if @pieces.any? { |pc| pc.current_square == from }
+			piece = @pieces.select { |pc| pc.current_square == from }[0]
+			if !piece.potential_moves.empty?
+				puts "Where would you like to move this piece?"
+				toalpha = gets.chomp; to = convert_coord(toalpha)
+				move_piece(from, to)
+			else
+				puts "This piece has no available moves. Please choose another piece."
+				turn
+			end
+		else
+			puts "Please choose a square occupied by a #{@color} piece."
+			turn
+		end
+	end
+
+	def convert_coord(coord)
+		split_coord = coord.downcase.split(//)
+		new_coord = []
+		if split_coord[0].between?('a','h') and split_coord[1].to_i.between?(1,8)
+			('a'..'h').to_a.each_with_index { |n, i| new_coord << i if n == split_coord[0] }
+			new_coord << (split_coord[1].to_i - 1)
+			new_coord
+		else
+			puts "That coordinate is not valid. Please try again."
+			turn
+		end
+	end
+
+	def subtract_captured(captured)
+		@pieces -= captured
 	end
 
 end
+
+
+
+
+=begin
+
+player1 = Player.new("white")
+player2 = Player.new("black")
+
+player2.pieces[8].current_square = [2, 2]
+player1.generate_moves
+player1.move_piece([1, 1], [2,2])
+
+player1.pieces.select { |pc| pc.class == Pawn }.each do |pc|
+	p "#{pc.class}: #{pc.current_square}, #{pc.first_move}"
+end
+
+p player1.captured
+
+
+
+
+	def turn
+		puts "#{@color.capitalize}\'s turn. Where is the piece you'd like to move? (e.g., 'A4', 'G2', etc.): "
+		fromalpha = gets.chomp; from = convert_coord(fromalpha)
+		if @pieces.any? { |pc| pc.current_square == from }
+			puts "Where would you like to move this piece?"
+			toalpha = gets.chomp; to = convert_coord(toalpha)
+			move_piece(from, to)
+		else
+			puts "Please choose a square occupied by a #{@color} piece."
+			turn
+		end
+	end
+
+	def convert_coord(coord)
+		split_coord = coord.downcase.split(//)
+		new_coord = []
+		if split_coord[0].between?('a','h') and split_coord[1].to_i.between?(1,8)
+			('a'..'h').to_a.each_with_index { |n, i| new_coord << i if n == split_coord[0] }
+			new_coord << (split_coord[1].to_i - 1)
+			new_coord
+		else
+			puts "That coordinate is not valid. Please try again."
+			turn
+		end
+	end
+
+=end 
