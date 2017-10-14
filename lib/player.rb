8228168c7eafa8a046cd14bdbad42d76e6bce06b
Chelsi Jones <cajones.bz@gmail.com>
2017-10-14 12:08:44 -0500

    Add hint for potential moves

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,13 @@
 require_relative 'pieces'
 
 class Player
-	attr_accessor :color, :pieces, :captured#, :board
+	attr_accessor :color, :pieces, :captured
 
 	def initialize(color)
 		@color = color
 		@pieces = []; @captured = []
 		@color == "white" ? generate_pieces(0, 1) : generate_pieces(7, 6)
 		generate_moves
-		#match_board(board)
 	end
 
 	def generate_pieces(a, b)
@@ -21,14 +20,41 @@ class Player
 
 
 	def generate_moves
-		@pieces.each do |pc|
-			pc.queen_moves if pc.class == Queen
-			pc.king_moves if pc.class == King
-			pc.rook_moves if pc.class == Rook
-			pc.knight_moves if pc.class == Knight
-			pc.bishop_moves if pc.class == Bishop
-			pc.pawn_moves if pc.class == Pawn
+		@pieces.each { |pc| pc.set_moves }
+	end
+
+
+	def castling?(king, to)
+		y = king.current_square[1]
+		if king.first_move and king.potential_moves.include? ([1,y] or [6,y])
+			rook1 = ChessPiece.all.select { |pc| pc.class == Rook and pc.current_square == [0, y] }[0]
+			rook2 = ChessPiece.all.select { |pc| pc.class == Rook and pc.current_square == [7, y] }[0]
+			rook1.current_square = [2,y] if to == [1,y]
+			rook2.current_square = [5,y] if to == [6,y]
 		end
+	end
+
+	def convert_coord(coord)
+		split_coord = coord.downcase.split(//)
+		num_coord = []
+		if split_coord[0].between?('a','h') and split_coord[1].to_i.between?(1,8)
+			('a'..'h').to_a.each_with_index { |n, i| num_coord << i if n == split_coord[0] }
+			num_coord << (split_coord[1].to_i - 1)
+			num_coord
+		else
+			false
+		end
+	end
+
+	def hint(movepc)
+		alphanums = []
+		movepc.potential_moves.each do |coord|
+			anc = ""
+			('a'..'h').to_a.each_with_index { |let, i| anc << let.upcase if coord[0] == i }
+			anc << (coord[1]+1).to_s
+			alphanums << anc
+		end
+		puts "Potential moves for #{movepc.class}: #{alphanums.join(", ")}"
 	end
 
 	def move_piece(from, to)
@@ -43,20 +69,16 @@ class Player
 			movepc.current_square = to
 			movepc.first_move = false if movepc.class == (Pawn || Rook || King)
 		else
-			puts "Please choose a valid move." # maybe have a hint option that shows possible moves?
-			puts "Potential_moves: #{movepc.potential_moves.inspect}"
-			turn
+			puts "Invalid move. Would you like to see potential moves? (Y/N)"
+			answer = gets.chomp.downcase
+			hint(movepc) if answer[0] == "y"; turn
+			#puts "Potential_moves: #{movepc.potential_moves.inspect}"
+			#turn
 		end
 	end
 
-	def castling?(king, to)
-		y = king.current_square[1]
-		if king.first_move and king.potential_moves.include? ([1,y] or [6,y])
-			rook1 = ChessPiece.all.select { |pc| pc.class == Rook and pc.current_square == [0, y] }[0]
-			rook2 = ChessPiece.all.select { |pc| pc.class == Rook and pc.current_square == [7, y] }[0]
-			rook1.current_square = [2,y] if to == [1,y]
-			rook2.current_square = [5,y] if to == [6,y]
-		end
+	def subtract_captured(captured)
+		@pieces -= captured
 	end
 
 	def turn
@@ -69,30 +91,12 @@ class Player
 				toalpha = gets.chomp; to = convert_coord(toalpha)
 				move_piece(from, to)
 			else
-				puts "This piece has no available moves. Please choose another piece."
-				turn
+				puts "This piece has no available moves. Please choose another piece."; turn
 			end
 		else
-			puts "Please choose a square occupied by a #{@color} piece."
+			puts from == false ? "That coordinate is not valid. Please try again." : "Please choose a square occupied by a #{@color} piece."
 			turn
 		end
-	end
-
-	def convert_coord(coord)
-		split_coord = coord.downcase.split(//)
-		new_coord = []
-		if split_coord[0].between?('a','h') and split_coord[1].to_i.between?(1,8)
-			('a'..'h').to_a.each_with_index { |n, i| new_coord << i if n == split_coord[0] }
-			new_coord << (split_coord[1].to_i - 1)
-			new_coord
-		else
-			puts "That coordinate is not valid. Please try again."
-			turn
-		end
-	end
-
-	def subtract_captured(captured)
-		@pieces -= captured
 	end
 
 end
@@ -101,47 +105,5 @@ end
 
 
 =begin
-
-player1 = Player.new("white")
-player2 = Player.new("black")
-
-player2.pieces[8].current_square = [2, 2]
-player1.generate_moves
-player1.move_piece([1, 1], [2,2])
-
-player1.pieces.select { |pc| pc.class == Pawn }.each do |pc|
-	p "#{pc.class}: #{pc.current_square}, #{pc.first_move}"
-end
-
-p player1.captured
-
-
-
-
-	def turn
-		puts "#{@color.capitalize}\'s turn. Where is the piece you'd like to move? (e.g., 'A4', 'G2', etc.): "
-		fromalpha = gets.chomp; from = convert_coord(fromalpha)
-		if @pieces.any? { |pc| pc.current_square == from }
-			puts "Where would you like to move this piece?"
-			toalpha = gets.chomp; to = convert_coord(toalpha)
-			move_piece(from, to)
-		else
-			puts "Please choose a square occupied by a #{@color} piece."
-			turn
-		end
-	end
-
-	def convert_coord(coord)
-		split_coord = coord.downcase.split(//)
-		new_coord = []
-		if split_coord[0].between?('a','h') and split_coord[1].to_i.between?(1,8)
-			('a'..'h').to_a.each_with_index { |n, i| new_coord << i if n == split_coord[0] }
-			new_coord << (split_coord[1].to_i - 1)
-			new_coord
-		else
-			puts "That coordinate is not valid. Please try again."
-			turn
-		end
-	end
 
 =end 
